a468b3089d9fbc16d55ea55338afb4b3de8ccebb
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-04-19 20:56:35 +0100

    added navbar react icons

--- react-portfolio/src/components/nav/Nav.jsx
+++ react-portfolio/src/components/nav/Nav.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import './nav.css';
+import React from "react";
+import "./nav.css";
+import { FaHome, FaRegUser } from "react-icons/fa";
+import { IoBookOutline } from "react-icons/io5";
+import { RiServiceLine } from "react-icons/ri";
+import { MdMessage } from "react-icons/md";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <nav>
+      <a href="/">
+        <FaHome />
+      </a>
+      <a href="#about">
+        <FaRegUser />
+      </a>
+      <a href="#experience">
+        <IoBookOutline />
+      </a>
+      <a href="#services">
+        <RiServiceLine />
+      </a>
+      <a href="#contact">
+        <MdMessage />
+      </a>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;

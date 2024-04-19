300ab1d2caa6eceae9170644b7c22ec670ba5ecb
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-04-19 15:22:33 +0100

    added the react-icons library to project

--- react-portfolio/src/components/header/Header.jsx
+++ react-portfolio/src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
+import ME from "../../assets/me.png";
 
 const Header = () => {
   return (
@@ -10,9 +11,17 @@ const Header = () => {
         <h1>Hajia Bintu</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
       </div>
     </header>
-  ); 
+  );
 };
 
 export default Header;
---
+++ react-portfolio/src/components/header/HeaderSocials.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FaLinkedin } from "react-icons/fa";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://linkedin.com" target="__blank"></a>
+      <a href="https://github.com" target="__blank"></a>
+      <a href="https://dribble.com" target="__blank"></a>
+    </div>
+  );
+}
+
+export default HeaderSocials

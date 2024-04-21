6b75f9adca48672b2c31104291c97b5d09b65a43
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-04-21 22:52:16 +0100

    css styling for contact component done

--- react-portfolio/src/components/contact/Contact.jsx
+++ react-portfolio/src/components/contact/Contact.jsx
@@ -13,15 +13,15 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineMail />
+            <MdOutlineMail className="contact__option-icon" />
             <h4>Email</h4>
-            <h5>kamaluddeenmuhamad585@gmail.com</h5>
+            <h5>meetkamal256@gmail.com</h5>
             <a href="mailto:kamaluddeenmuhamad585@gmail.com" target="__blank">
               Send a message
             </a>
           </article>
           <article className="contact__option">
-            <FaFacebook />
+            <FaFacebook className="contact__option-icon" />
             <h4>Messenger</h4>
             <h5>Facebook</h5>
             <a href="https://m.me/kamal.muhammad.50552338" target="__blank">
@@ -29,7 +29,7 @@ const Contact = () => {
             </a>
           </article>
           <article className="contact__option">
-            <FaWhatsapp />
+            <FaWhatsapp className="contact__option-icon" />
             <h4>+2348164682518</h4>
             <h5>Whatsapp</h5>
             <a href="https://wa.me/2348164682518" target="_blank">

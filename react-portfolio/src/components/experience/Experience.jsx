e0452fa35519b604cd78900531f2744a2d29a529
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-04-21 11:29:28 +0100

    experience component css styling done

--- react-portfolio/src/components/experience/Experience.jsx
+++ react-portfolio/src/components/experience/Experience.jsx
@@ -12,42 +12,42 @@ const Experience = () => {
           <h3>Frontend Development</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <HiCheckBadge className="experience__details-icon"/>
+              <HiCheckBadge className="experience__details-icon" />
               <div>
                 <h4>HTML</h4>
                 <small>Experienced</small>
               </div>
             </article>
             <article className="experience__details">
-              <HiCheckBadge className="experience__details-icon"/>
+              <HiCheckBadge className="experience__details-icon" />
               <div>
                 <h4>CSS</h4>
                 <small>Intermediate</small>
               </div>
             </article>
             <article className="experience__details">
-              <HiCheckBadge className="experience__details-icon"/>
+              <HiCheckBadge className="experience__details-icon" />
               <div>
                 <h4>Javascript</h4>
                 <small>Experienced</small>
               </div>
             </article>
             <article className="experience__details">
-              <HiCheckBadge className="experience__details-icon"/>
+              <HiCheckBadge className="experience__details-icon" />
               <div>
                 <h4>Tailwind css</h4>
                 <small>Experienced</small>
               </div>
             </article>
             <article className="experience__details">
-              <HiCheckBadge className="experience__details-icon"/>
+              <HiCheckBadge className="experience__details-icon" />
               <div>
                 <h4>Bootstrap</h4>
                 <small>Experienced</small>
               </div>
             </article>
             <article className="experience__details">
-              <HiCheckBadge className="experience__details-icon"/>
+              <HiCheckBadge className="experience__details-icon" />
               <div>
                 <h4>React js</h4>
                 <small>Experienced</small>
@@ -55,40 +55,40 @@ const Experience = () => {
             </article>
           </div>
         </div>
-        
+
         <div className="experience__backend">
           <h3>Backend Development</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <HiCheckBadge className="experience__details-icon"/>
+              <HiCheckBadge className="experience__details-icon" />
               <div>
                 <h4>Node js</h4>
                 <small>Intermediate</small>
               </div>
             </article>
             <article className="experience__details">
-              <HiCheckBadge className="experience__details-icon"/>
+              <HiCheckBadge className="experience__details-icon" />
               <div>
                 <h4>MongoDB</h4>
                 <small>Intermediate</small>
               </div>
             </article>
             <article className="experience__details">
-              <HiCheckBadge className="experience__details-icon"/>
+              <HiCheckBadge className="experience__details-icon" />
               <div>
                 <h4>PHP</h4>
                 <small>Intermediate</small>
               </div>
             </article>
             <article className="experience__details">
-              <HiCheckBadge className="experience__details-icon"/>
+              <HiCheckBadge className="experience__details-icon" />
               <div>
                 <h4>MYSQL</h4>
                 <small>Basic</small>
               </div>
             </article>
             <article className="experience__details">
-              <HiCheckBadge className="experience__details-icon"/>
+              <HiCheckBadge className="experience__details-icon" />
               <div>
                 <h4>Python</h4>
                 <small>Experienced</small>

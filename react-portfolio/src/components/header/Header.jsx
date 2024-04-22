c2a93a454cdf2b07908c18d4e068f11c8785e2ef
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-04-22 01:37:33 +0100

    modified header component jsx

--- react-portfolio/src/components/header/Header.jsx
+++ react-portfolio/src/components/header/Header.jsx
@@ -13,11 +13,11 @@ const Header = () => {
         <h5 className="text-light">Frontend Developer</h5>
         <CTA />
         <HeaderSocials />
-        
+
         <div className="me">
           <img src={ME} alt="me" />
         </div>
-        
+
         <a href="#contact" className="scroll__down">
           Scroll Down
         </a>

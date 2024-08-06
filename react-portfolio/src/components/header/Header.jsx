49075178217d07eff4584590d4133221289a614d
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-08-06 18:32:35 +0100

    modified header component styling

--- react-portfolio/src/components/header/Header.jsx
+++ react-portfolio/src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
-import ME from "../../assets/kamal-pic.jpeg";
+import ME from "../../assets/kamal.png";
 import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
